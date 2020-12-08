0df50f164deece453d67d5a491ab87c44e346962
catherineblu <e.ropleva@gmail.com>
2020-12-08 14:42:09 +0200

    added the functionality of the 'Read more' button :)

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const herb_info = document.getElementById('herb_info');
+
+open.addEventListener('click', () => {
+    herb_info.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    herb_info.classList.remove('show');
+});
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
